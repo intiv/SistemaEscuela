6b5d4eba8d3504f2ffebdeb80db5b2aa3026e8bb
joseluisfiallosguerrero1 <jose.fiallos@unitec.edu>
2017-06-09 22:29:01 -0600

    probando

--- sistemaescuela/backend/schemas/homework.js
+++ sistemaescuela/backend/schemas/homework.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
 
 var HomeworkSchema = new mongoose.Schema({
-	id : Number,
-	grade : {type : Number, required : true},
-	title : {type : String, required : true},
-	description : String,
-	teacher : Number,
-	class : {type : Number, required : true},
-	score : Number
+	id : String,
+	seccion: String,
+	titulo: String,
+	descripcion: String,
+	fecha_de_envio: Date,
+	fecha_de_entrega: Date,
+	porcentaje_obtenido: Number,
+	parcial: Number,
+	valor:Number
 });
 
 module.exports = mongoose.model('homeworks', HomeworksSchema);
