32bf76a06d8f12030f512d2c59d37b6ffd1b662d
Inti Velasquez <intiv@github.com>
2017-06-10 21:16:08 -0600

    Terminado y probado

--- sistemaescuela/backend/routes/routes.js
+++ sistemaescuela/backend/routes/routes.js
@@ -2,77 +2,133 @@ var boom = require('boom');
 var joi = require('joi');
 var uuid = require('node-uuid');
 var user = require('../controllers/usersController');
+var seccion = require('../controllers/seccionController');
 
 exports.endpoints = [
 	{
 		method: 'GET',
-		path: '/users',
+		path: '/usuarios',
 		config: user.getAllUsers
 		
 	},
+
 	{
 		method: 'POST',
-		path: '/users',
+		path: '/ususarios/crear',
 		config: user.createUser
 	},
+
 	{
 		method: 'PUT',
-		path: '/users/{id}',
+		path: '/usuarios/modificar/{id}',
 		config: user.modifyUser
 	},
+
 	{
 		method: 'DELETE',
-		path: '/users/{id}',
+		path: '/usuarios/borrar/{id}',
 		config: user.deleteUser
 	},
-	{
-		method: 'POST',
-		path: '/students',
-		config: user.createStudent
-	},
+
 	{
 		method: 'GET',
-		path: '/students',
-		config: user.getAllStudents
-	},
-	{
-		method: 'PUT',
-		path: '/students/{id}',
-		config: user.modifyStudent
-	},
-	{
-		method: 'DELETE',
-		path: '/students/{id}',
-		config: user.deleteStudent
-	},
-	{
-		method: 'GET',
-		path: '/students/search/id/{id}',
-		config: user.getStudentById
-	},
-	{
-		method: 'GET',
-		path: '/users/search/id/{id}',
-		config: user.getUserById
-	},
-	{
-		method: 'GET',
-		path: '/students/search/account/{cuenta}',
-		config: user.getStudentByAccount
-	},
-	{
-		method: 'GET',
-		path: '/students/search/name/{nombre}',
-		config: user.getStudentsByName
-	},
-	{
-		method: 'GET',
-		path: '/users/teachers',
+		path: '/usuarios/maestros',
 		config: user.getTeachers
 	},
+
 	{
 		method: 'GET',
-		path: '/users/teachers/{id}',
+		path: '/usuarios/maestros/{id}',
 		config: user.getTeacherById
+	},
+
+	{
+		method: 'GET',
+		path: '/usuarios/buscar/id/{id}',
+		config: user.getUserById
+	},
+	//alumnos
+	{
+		method: 'POST',
+		path: '/alumnos/crear',
+		config: user.createStudent
+	},
+
+	{
+		method: 'GET',
+		path: '/alumnos',
+		config: user.getAllStudents
+	},
+
+	{
+		method: 'PUT',
+		path: '/alumnos/modificar/{id}',
+		config: user.modifyStudent
+	},
+
+	{
+		method: 'DELETE',
+		path: '/alumnos/borrar/{id}',
+		config: user.deleteStudent
+	},
+
+	{
+		method: 'GET',
+		path: '/alumnos/buscar/id/{id}',
+		config: user.getStudentById
+	},
+
+	{
+		method: 'GET',
+		path: '/alumnos/buscar/cuenta/{cuenta}',
+		config: user.getStudentByAccount
+	},
+
+	{
+		method: 'GET',
+		path: '/alumnos/buscar/nombre/{nombre}',
+		config: user.getStudentsByName
+	},
+	//secciones
+	{
+		method: 'GET',
+		path: '/secciones',
+		config: seccion.getAllSections
+	},
+
+	{
+		method: 'GET',
+		path: '/secciones/buscar/id/{id}',
+		config: seccion.getSectionById
+	},
+
+	{
+		method: 'GET',
+		path: '/secciones/buscar/maestro/{maestro}',
+		config: seccion.getSectionsByTeacher
+	},
+
+	{
+		method: 'GET',
+		path: '/secciones/buscar/grado/{grado}',
+		config: seccion.getSectionsByGrade
+	},
+
+	{
+		method: 'GET',
+		path: '/secciones/buscar/apartado/{apartado}',
+		config: seccion.getSectionsByApartado
+	},
+
+	{
+		method: 'PUT',
+		path: '/secciones/modificar/{id}',
+		config: seccion.modifySection
+	},
+
+	{
+		method: 'POST',
+		path: '/secciones/crear',
+		config: seccion.createSection
 	}
 ];
